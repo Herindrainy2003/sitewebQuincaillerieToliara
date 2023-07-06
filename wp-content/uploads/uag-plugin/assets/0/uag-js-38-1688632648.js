f7412b283b460315dda1876bb99024b788009d65
herindrainy2003 <herindrainyall2003@gmail.com>
2023-07-06 15:23:33 +0300

    j-11

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-38-1688632648.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-6ee8f9ea' );
+	UAGBTabs.anchorTabId( '.uagb-block-6ee8f9ea' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-6ee8f9ea' );
+}, false );
+ });
