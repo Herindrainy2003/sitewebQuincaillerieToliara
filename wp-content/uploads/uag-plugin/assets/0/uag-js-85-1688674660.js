cddf0b2a221fc90a14857e892d5e27930283d770
herindrainy2003 <herindrainyall2003@gmail.com>
2023-07-07 15:31:35 +0300

    jj

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-85-1688674660.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-90b155e6' );
+	UAGBTabs.anchorTabId( '.uagb-block-90b155e6' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-90b155e6' );
+}, false );
+ });
