b7287c2358218216d702d9392df047d41a48409e
herindrainy2003 <herindrainyall2003@gmail.com>
2023-07-10 08:46:52 +0300

    j2

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-41-1688967451.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-90b155e6' );
+	UAGBTabs.anchorTabId( '.uagb-block-90b155e6' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-90b155e6' );
+}, false );
+window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-fd14f8a5' );
+	UAGBTabs.anchorTabId( '.uagb-block-fd14f8a5' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-fd14f8a5' );
+}, false );
+ });
